884378053b8ddf5168ff54ecd73ff8b68bdffbc4
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2021-11-17 09:20:16 -0500

    feat: some minor cleanup

--- shared/world.ts
+++ shared/world.ts
@@ -28,9 +28,7 @@ export type DisplayState = Snowglobe.DisplayState & {
 const ENTITY_COUNT = 1_000
 const TIMESTEP = 1 / 60
 const GRAVITY = { x: 0, y: -9.81, z: 0 }
-const GROUND_SNAP_DISTANCE = 0.5
-const GROUND_SNAP_EASING = 0.5
-const HORIZONTAL_DAMPING = 0.2
+const DAMPING_GROUND = 0.15
 const PLAYER_HEIGHT = 3
 const PLAYER_RADIUS = 1
 
@@ -81,15 +79,24 @@ function hasInput(mask: number, inputs: PlayerInput) {
   return (mask & inputs) === inputs
 }
 
-const GROUND_CAST_SHAPE = new Rapier.Capsule(PLAYER_HEIGHT / 2, PLAYER_RADIUS)
-const WALL_CAST_SHAPE = new Rapier.Capsule(PLAYER_HEIGHT / 3, PLAYER_RADIUS)
+const CAST_SHAPE_V = new Rapier.Capsule(PLAYER_HEIGHT * 0.5, PLAYER_RADIUS)
+const CAST_SHAPE_H = new Rapier.Capsule(PLAYER_HEIGHT * 0.49, PLAYER_RADIUS)
+
+function normalize(vector: Rapier.Vector3, out = vector) {
+  const { x, y, z } = vector
+  const length = Math.sqrt(x * x + y * y)
+  out.x = x / length
+  out.y = y / length
+  out.z = z / length
+  return out
+}
 
 function detectKinematicCollision(
   simulation: Rapier.World,
   body: Rapier.RigidBody,
   forceVector: Rapier.Vector3,
-  shape = GROUND_CAST_SHAPE,
-  distance = PLAYER_HEIGHT / 2,
+  shape: Rapier.Shape,
+  distance: number,
 ) {
   const translation = body.translation()
   const rotation = body.rotation()
@@ -185,46 +192,56 @@ export function make(): World {
           const down = +hasInput(i, PlayerInput.Down)
           const left = +hasInput(i, PlayerInput.Left)
           const right = +hasInput(i, PlayerInput.Right)
+          const hmov = normalize(new Rapier.Vector3(right - left, 0, down - up))
 
-          let targetX = (force.x[k] + (right - left) * 0.05) * (1 - HORIZONTAL_DAMPING)
-          let targetZ = (force.z[k] + (down - up) * 0.05) * (1 - HORIZONTAL_DAMPING)
-          let targetY = force.y[k] - 9.81 * 0.002
+          let targetForceX = force.x[k]
+          let targetForceY = force.y[k] - 9.81 * 0.002 // gravity
+          let targetForceZ = force.z[k]
 
-          const v = detectKinematicCollision(
+          // handle vertical (y-axis) collisions
+          const toiVertical = detectKinematicCollision(
             simulation,
             b,
-            new Rapier.Vector3(0, targetY, 0),
+            new Rapier.Vector3(0, targetForceY, 0),
+            CAST_SHAPE_V,
+            PLAYER_HEIGHT / 2,
           )
-          const h = detectKinematicCollision(
+
+          const grounded = toiVertical && Math.abs(toiVertical.normal1.y) > 0
+
+          if (grounded) {
+            // body is grounded – remove y forces and apply player input
+            targetForceY = hasInput(i, PlayerInput.Jump) ? 0.3 : 0
+            targetForceX = (force.x[k] + (right - left) * 0.05) * (1 - DAMPING_GROUND)
+            targetForceZ = (force.z[k] + (down - up) * 0.05) * (1 - DAMPING_GROUND)
+          }
+          // TODO: mid-air motion (to help get over obstacles)
+
+          // handle horizontal (x,z-axis) collisions
+          const toiHorizontal = detectKinematicCollision(
             simulation,
             b,
-            new Rapier.Vector3(targetX, 0, targetZ),
-            WALL_CAST_SHAPE,
-            PLAYER_RADIUS * 3,
+            new Rapier.Vector3(targetForceX, 0, targetForceZ),
+            CAST_SHAPE_H,
+            PLAYER_RADIUS,
           )
 
-          if (v && Math.abs(v.normal1.y) > 0) {
-            targetY = 0
+          if (toiHorizontal) {
+            // inhibit movement along axis where collision is occurring
+            const { x, z } = toiHorizontal.normal1
+            if (Math.abs(x) > 0.1) targetForceX = 0
+            if (Math.abs(z) > 0.1) targetForceZ = 0
           }
 
-          if (h && Math.abs(h.normal1.x) > 0) {
-            targetX = 0
-          }
+          // integrate the updated forces
+          t.x += force.x[k] = targetForceX
+          t.y += force.y[k] = targetForceY
+          t.z += force.z[k] = targetForceZ
 
-          if (h && Math.abs(h.normal1.z) > 0) {
-            targetZ = 0
-          }
-
-          if (targetY === 0 && hasInput(i, PlayerInput.Jump)) {
-            targetY = 0.3
-          }
-
-          t.x += force.x[k] = targetX
-          t.y += force.y[k] = targetY
-          t.z += force.z[k] = targetZ
-
+          // disable repeating jump input
           input[k] &= ~PlayerInput.Jump
 
+          // update kinematic body
           b.setNextKinematicTranslation(t)
         }
       }
